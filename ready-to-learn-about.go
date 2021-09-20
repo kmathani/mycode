42d3b6426dc5c35190e55a325b1637d7120802cc
Shiek Kamaal <kmathani@users.noreply.github.com>
2021-09-20 17:54:02 +0000

    challenge 01 excercise

---
+++ ready-to-learn-about.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Looks like you are ready to learn GoLang with Alta3 Research")
+	fmt.Println("My favourite number is 10")
+	fmt.Println("However, I grab random number as well", rand.Intn(10))
+
+}
+
